8c71c082a0d809dcef2f94cb9cada22decd2b456
David Edwards <david.edwards@hyland.com>
2021-12-17 07:04:21 -0500

    ACS-2201 Add new endpoints for storage-info
    Models generated from swagger at branch:
    feature/ACS-2336_POST_endpoints_archive_restore_content
    
    Endpoints added:
    GET nodes/{nodeId}/storage-info/{contentPropName}
    POST nodes/{nodeId}/storage-info/{contentPropName}/archive
    POST nodes/{nodeId}/storage-info/{contentPropName}/archive-restore

---
+++ src/main/java/org/alfresco/rest/requests/ContentStorageInformation.java
@@ -0,0 +1,131 @@
+package org.alfresco.rest.requests;
+
+import org.alfresco.rest.core.RestRequest;
+import org.alfresco.rest.core.RestResponse;
+import org.alfresco.rest.core.RestWrapper;
+import org.alfresco.rest.model.RestArchiveContentRequestModel;
+import org.alfresco.rest.model.RestContentStorageInfoModel;
+import org.alfresco.rest.model.RestRestoreArchivedContentRequestModel;
+import org.alfresco.utility.model.TestModel;
+import org.springframework.http.HttpMethod;
+
+public class ContentStorageInformation extends ModelRequest<ContentStorageInformation> 
+{
+    private static final String basePath = "nodes/{nodeId}/storage-info/{contentPropName}";
+    RestContentStorageInfoModel storageInfoModel;
+
+    private String nodeId;
+    private String contentPropName;
+
+    public ContentStorageInformation(RestWrapper restWrapper) 
+    {
+        super(restWrapper);
+    }
+
+
+    public ContentStorageInformation withNodeId(String nodeId)
+    {
+        this.nodeId = nodeId;
+        return this;
+    }
+
+    public ContentStorageInformation withContentPropName(String contentPropName)
+    {
+        this.contentPropName = contentPropName;
+        return this;
+    }
+
+    private void verifyNodeIdAndPropName()
+    {
+        if (nodeId == null || contentPropName == null) 
+        {   
+            throw new IllegalArgumentException("nodeId and contentPropName must be initialised.");
+        }
+    }
+
+    /**
+     * Get Content Storage Properties using GET call on "nodes/{nodeId}/storage-info/{contentPropName}"
+     * @return
+     */
+    public RestContentStorageInfoModel getStorageInfo()
+    {
+        verifyNodeIdAndPropName();
+        return getStorageInfo(nodeId, contentPropName);
+    }
+
+    /**
+     * Get Content Storage Properties using GET call on "nodes/{nodeId}/storage-info/{contentPropName}"
+     * @param nodeId The nodeId
+     * @param contentPropName The content property QNAME ie. "cm:content"
+     * @return
+     */
+    public RestContentStorageInfoModel getStorageInfo(String nodeId, String contentPropName)
+    {
+        RestRequest request = RestRequest.simpleRequest(HttpMethod.GET, basePath, nodeId, contentPropName);
+        return restWrapper.processModel(RestContentStorageInfoModel.class, request);
+    }
+
+    /**
+     * Send content to archive using POST call on "nodes/{nodeId}/storage-info/{contentPropName}/archive"
+     * 
+     * @param archiveContentRequest The request body
+     * @return
+     */
+    public RestResponse requestArchiveContent(RestArchiveContentRequestModel archiveContentRequest)
+    {
+        verifyNodeIdAndPropName();
+        return requestArchiveContent(nodeId, contentPropName, archiveContentRequest);
+    }
+
+    /**
+     * Send content to archive using POST call on "nodes/{nodeId}/storage-info/{contentPropName}/archive"
+     * 
+     * @param nodeId The nodeId
+     * @param contentPropName The content property QNAME ie. "cm:content"
+     * @param archiveContentRequest The request body
+     * @return
+     */
+    public RestResponse requestArchiveContent(String nodeId, String contentPropName, RestArchiveContentRequestModel archiveContentRequest)
+    {
+        RestRequest request = createRestRequestWithBody(HttpMethod.POST, archiveContentRequest, basePath + "/archive", nodeId, contentPropName);
+        return restWrapper.process(request);
+    }
+
+    /**
+     * Send content to archive using POST call on "nodes/{nodeId}/storage-info/{contentPropName}/archive-restore"
+     * 
+     * @param restoreArchivedContentRequest The request body
+     * @return
+     */
+    public RestResponse requestRestoreContentFromArchive(RestRestoreArchivedContentRequestModel restoreArchivedContentRequest)
+    {
+        verifyNodeIdAndPropName();
+        return requestRestoreContentFromArchive(nodeId, contentPropName, restoreArchivedContentRequest);
+    }
+
+    /**
+     * Send content to archive using POST call on "nodes/{nodeId}/storage-info/{contentPropName}/archive-restore"
+     * 
+     * @param nodeId The nodeId
+     * @param contentPropName The content property QNAME ie. "cm:content"
+     * @param restoreArchivedContentRequest The request body
+     * @return
+     */
+    public RestResponse requestRestoreContentFromArchive(String nodeId, String contentPropName, RestRestoreArchivedContentRequestModel restoreArchivedContentRequest)
+    {
+        RestRequest request = createRestRequestWithBody(HttpMethod.POST, restoreArchivedContentRequest, basePath + "/archive-restore", nodeId, contentPropName);
+        return restWrapper.process(request);
+    }
+    
+    private RestRequest createRestRequestWithBody(HttpMethod httpMethod, TestModel request, String path, String... pathParams)
+    {
+        if (request != null)
+        {
+            return RestRequest.requestWithBody(httpMethod, request.toJson(), path, pathParams);
+        }
+        else
+        {
+            return RestRequest.simpleRequest(httpMethod, path, pathParams);
+        }
+    }
+}
